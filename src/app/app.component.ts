ca39f8bfca61d12d6c9011eb2837eca7d1f0a81e
Akhmedov Isa <akhmedovisa167@gmail.com>
2026-05-23 11:26:20 +0500

    feat: complete homework 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import './training';
+import { Colors } from '../enums/Color';
+import './collection';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,37 @@ import './training';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
+  
+  companyName: string = 'румтибет';
+  
+  isRgbColor(color: Colors): boolean {
+    return color === Colors.RED || color === Colors.GREEN || color === Colors.BLUE;
+  }
+  
+  saveLastVisitDate() {
+    const lastVisit = localStorage.getItem('lastPageVisit');
+    
+    if (lastVisit) {
+      console.log(`С возвращением, вам последний визит был ${lastVisit}`)
+    } else {
+      console.log('Добро пожаловать! Это ваш превый визит');
+    }
+    
+    const now = new Date().toLocaleString();
+    localStorage.setItem('lastPageVisit', now);
+  }
+  
+  quantityVisits() {
+    const currentVisits = Number(localStorage.getItem('numberVisits')) || 1;
+    
+    if(currentVisits > 1) {
+      console.log(`Приветствую вас снова, это ваш ${currentVisits} визит`);
+    } else {
+      console.log('Добро пожаловать, это ваш первый визит');
+    }
+    
+    localStorage.setItem('numberVisits', (currentVisits+1).toString())
+  }
 }
+
+
---
+++ src/enums/Color.ts
@@ -0,0 +1,7 @@
+export enum Colors {
+  GREEN = 'GREEN',
+  RED = 'RED',
+  BLUE = 'BLUE',
+  YELLOW = 'YELLOW',
+  BLACK = 'BLACK'
+}
